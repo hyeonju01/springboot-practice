5a567a4595d1fa4b9c8e4d3a523a9f777192c7c5
hyeonju01 <tangled6648@gmail.com>
2022-10-27 11:49:57 +0900

    22. 10. 25. SpringBoot 첫 수업 - QueryParam Map 완료

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,13 +1,39 @@
 package com.springboot.hello.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api/v1/hello")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("")
     public String Hello() {
         return "Hello World";
     }
+
+    @GetMapping(value = "/request1") //endpoint
+    public String getVariable2(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String organization) {
+        return String.format(" %s %s %s", name, email, organization);
+    }
+
+    @GetMapping (value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+//            sb.append(map.getKey() + " " + map.getValue() + "\n" );
+            System.out.printf("key: %s value: %s", map.getKey(), map.getValue());
+        });
+        return "request2가 호출 완료되었습니다.";
+
+    }
+
+
+
 }
